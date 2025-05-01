8f65f046b80e2127813d614bd7f28b06d5147202
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2025-05-01 12:18:00 -0400

    master commit

---
+++ src/main/java/com/prod/todo/TodoProdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prod.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoProdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoProdServiceApplication.class, args);
+	}
+
+}

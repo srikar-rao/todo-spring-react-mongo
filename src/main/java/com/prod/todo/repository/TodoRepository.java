9da4289b715a4d566851e3ab81f10638f58649d6
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2025-05-11 12:56:11 -0400

    Jpa read only query hints with custom annotation

--- src/main/java/com/prod/todo/repository/TodoRepository.java
+++ src/main/java/com/prod/todo/repository/TodoRepository.java
@@ -1,9 +1,12 @@
 package com.prod.todo.repository;
 
+import com.prod.todo.annotation.ReadOnlyQuery;
 import com.prod.todo.entity.TodoEntity;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,9 +15,11 @@ import java.util.List;
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
 
     @Query("SELECT t FROM TodoEntity t LEFT JOIN FETCH t.tasks")
+    @ReadOnlyQuery
     List<TodoEntity> findAllWithTasks();
 
     @EntityGraph(attributePaths = {"tasks"})
+    @ReadOnlyQuery
     @Query("SELECT t FROM TodoEntity t")
     List<TodoEntity> findAllWithTasksUsingGraph();
 

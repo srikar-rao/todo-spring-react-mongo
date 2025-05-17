dd6887fcffcdc09a1da38ee962250d137382d169
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2025-05-17 12:43:10 -0400

    create all apis services

--- src/api/services/todoService.ts
+++ src/api/services/todoService.ts
@@ -6,4 +6,23 @@ export const TodoService = {
         const res = await axiosTodo.get('/todo/all');
         return res.data;
       },
+
+      getById: async (id: number): Promise<Todo> => {
+        const res = await axiosTodo.get(`/todo/${id}`);
+        return res.data;
+      },
+    
+      create: async (todo: Partial<Todo>): Promise<Todo> => {
+        const res = await axiosTodo.post('/todo/save', todo);
+        return res.data;
+      },
+    
+      update: async (todo: Partial<Todo>): Promise<Todo> => {
+        const res = await axiosTodo.put(`/todo/update`, todo);
+        return res.data;
+      },
+    
+      delete: async (id: number): Promise<void> => {
+        await axiosTodo.delete(`/todo/delete${id}`);
+      },
 }
